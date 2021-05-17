89b676d7841b801c3300acabb456414207a1ad50
Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>
2021-05-16 19:35:47 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // script.js
-
+/* eslint-disable */
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
-
+let id = 0;
 // Make sure you register your service worker here too
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,7 +12,29 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        newPost.id = id;
+        id += 1;
+        newPost.addEventListener('click', () => {
+          setState({state: 'entry', id: newPost.id})
+        })
         document.querySelector('main').appendChild(newPost);
       });
     });
+});
+
+
+document.querySelector('img').addEventListener('click', () => {
+  if(location.hash != '#settings'){
+    setState({state: 'settings'}, false);
+  }
+})
+
+document.querySelector('h1').addEventListener('click', () => {
+  if(location.hash != ''){
+    setState({state: 'home'}, false);
+  }
+})
+
+window.addEventListener('popstate', (event) => {
+  setState(event.state, true);
 });
